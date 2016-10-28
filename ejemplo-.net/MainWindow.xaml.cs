79d91d07dbf086e6190050bf145ac4d9bdf1eb13
SCM <SCM>
2016-10-28 14:21:33 -0700

    Commit Inicial

---
+++ ejemplo-.net/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Soap;
+
+
+namespace Main
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public Params webservice = new Params();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string path = Directory.GetCurrentDirectory();
+            FileStream fileStream = new FileStream(@path + "\\config.xml", FileMode.Open);
+            SoapFormatter formatter = new SoapFormatter();
+            webservice = (Params)formatter.Deserialize(fileStream);
+        }
+
+        private void button_timbrar_Click(object sender, RoutedEventArgs e)
+        {           
+            var acceso_servicio = new Servicios();
+            if (acceso_servicio.timbrar_cfdi(webservice.username, webservice.password, webservice.xmlbase64) == true)
+                MessageBox.Show("Termino el envío a timbrar el CFDI");
+            else
+                MessageBox.Show("Hubo un error en la petición, revisar la consola para mas información");
+        }
+
+        private void button_cancelar_Click(object sender, RoutedEventArgs e)
+        {
+            //leer y pasar a base64 el archivo pfx
+            string path = Directory.GetCurrentDirectory();
+            byte[] fileBytes = File.ReadAllBytes(@path + "\\archivo.pfx");
+            string base642 = Convert.ToBase64String(fileBytes);
+            webservice.pfxbase64 = base642;
+
+            var acceso_servicio = new Servicios();
+            if (acceso_servicio.cancelar_cfdi(webservice.username, webservice.password, webservice.rfc, webservice.uuid, webservice.pfxbase64, webservice.pfxpassword) == true)
+                MessageBox.Show("Termino el envío a cancelación del CFDI");
+            else
+                MessageBox.Show("Hubo un error en la petición, revisar la consola para mas información");
+        }
+    }
+}
---
+++ ejemplo-.net/Servicios.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Main
+{
+    class Servicios
+    {
+
+        public bool timbrar_cfdi(string user_name, string password, string xml_base64)
+        {
+            try
+            {
+                TimboxWS.timbrado_portClient cliente_timbrar = new TimboxWS.timbrado_portClient();
+                TimboxWS.timbrar_cfdi_result response = new TimboxWS.timbrar_cfdi_result();
+                response = cliente_timbrar.timbrar_cfdi(user_name, password, xml_base64);
+                XmlDocument cfdi_timbrado = new XmlDocument();
+                cfdi_timbrado.LoadXml(response.xml);
+                Console.WriteLine(response.xml);
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return false;
+            }
+        }
+
+        public bool cancelar_cfdi(string user_name, string password, string rfc, string uuid, string pfx_base64, string pfx_password)
+        {
+            try
+            {
+                string[] arr_uuids = new string[1] { uuid };
+                TimboxWS.uuid uuids = new TimboxWS.uuid();
+                uuids.uuid1 = arr_uuids;
+                TimboxWS.timbrado_port cliente_cancelar = new TimboxWS.timbrado_portClient();
+                TimboxWS.cancelar_cfdi_result response = new TimboxWS.cancelar_cfdi_result();
+                response = cliente_cancelar.cancelar_cfdi(user_name, password, rfc, uuids, pfx_base64, pfx_password);
+                XmlDocument acuse_cancelacion = new XmlDocument();
+                acuse_cancelacion.LoadXml(response.acuse_cancelacion);
+                XmlDocument comprobantes_cancelados = new XmlDocument();
+                comprobantes_cancelados.LoadXml(response.comprobantes_cancelados);
+                Console.WriteLine(response.ToString());
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return false;
+            }
+        }
+    }
+}
