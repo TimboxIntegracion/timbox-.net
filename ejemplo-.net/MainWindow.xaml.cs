164f5fd6fe361692c7f56e0c73e18dc75bd79a28
Developer <Developer>
2017-05-09 11:48:37 -0700

    Actualizacion al repositorio con el metodo sellar

--- ejemplo-.net/MainWindow.xaml.cs
+++ ejemplo-.net/MainWindow.xaml.cs
@@ -15,7 +15,7 @@ using System.Windows.Shapes;
 using System.IO;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Soap;
-
+using System.Diagnostics;
 
 namespace Main
 {
@@ -24,40 +24,143 @@ namespace Main
     /// </summary>
     public partial class MainWindow : Window
     {
-        public Params webservice = new Params();
+        //VARIABLES
+        private string userName = "AAA010101000";
+        private string password = "h6584D56fVdBbSmmnB";
+  
+        private string pfxBase64 = string.Empty;
+        private string pfxPassword = "12345678a";
+        private string fileStream = "\\archivoXml.xml";
+        string path = Directory.GetCurrentDirectory();
 
-        public MainWindow()
+        private string xmlBase64 = string.Empty;
+        private string respuesta_servicio = string.Empty;
+        private string respuesta_cancelar = string.Empty;
+
+        //PROPIEDADES
+        private string _rfcTextBox;
+        public string rfcTextBox
         {
-            InitializeComponent();
-
-            string path = Directory.GetCurrentDirectory();
-            FileStream fileStream = new FileStream(@path + "\\config.xml", FileMode.Open);
-            SoapFormatter formatter = new SoapFormatter();
-            webservice = (Params)formatter.Deserialize(fileStream);
+            get
+            {
+                return _rfcTextBox;
+            }
+            set
+            {
+                if (value != null)
+                    _rfcTextBox = value;
+            }
         }
 
+        private string _uuidTextBox;
+        public string uuidTextBox
+        {
+            get
+            {
+                return _uuidTextBox;
+            }
+            set
+            {
+                if (value != null)
+                    _uuidTextBox = value;
+            }
+        }
+
+        private string _ResultadoTextBox;
+        public string ResultadoTextBox
+        {
+            get
+            {
+                return _ResultadoTextBox;
+            }
+            set
+            {
+                if (value != null)
+                    _ResultadoTextBox = value;
+            }
+        }
+
+        private bool _TabTimbrarSelected;
+        public bool TabTimbrarSelected
+        {
+            get
+            {
+                return _TabTimbrarSelected;
+            }
+            set
+            {
+                _TabTimbrarSelected = value;
+            }
+        }
+
+        private bool _TabCancelarSelected;
+        public bool TabCancelarSelected
+        {
+            get
+            {
+                return _TabCancelarSelected;
+            }
+            set
+            {
+                _TabCancelarSelected = value;
+            }
+        }
+
+        //CONSTRUCTOR
+        public MainWindow()
+        {
+            DataContext = this;
+            InitializeComponent();
+        }
+
+        //EVENTOS
         private void button_timbrar_Click(object sender, RoutedEventArgs e)
-        {           
+        {
             var acceso_servicio = new Servicios();
-            if (acceso_servicio.timbrar_cfdi(webservice.username, webservice.password, webservice.xmlbase64) == true)
-                MessageBox.Show("Termino el envío a timbrar el CFDI");
+            string path_xml = path + fileStream;
+            xmlBase64 = acceso_servicio.sellar_cfdi(path_xml, path);
+
+            if (!string.IsNullOrEmpty(userName) && !string.IsNullOrEmpty(password) && !string.IsNullOrEmpty(xmlBase64))
+            {
+                respuesta_servicio = acceso_servicio.timbrar_cfdi(userName, password, xmlBase64);
+                TextBox_resultado.Text = respuesta_servicio.ToString();
+            }
             else
-                MessageBox.Show("Hubo un error en la petición, revisar la consola para mas información");
+                MessageBox.Show("Intente de nuevo.");
+
+            //Limpiar respuesta
+            respuesta_servicio = string.Empty;
         }
 
         private void button_cancelar_Click(object sender, RoutedEventArgs e)
         {
             //leer y pasar a base64 el archivo pfx
             string path = Directory.GetCurrentDirectory();
-            byte[] fileBytes = File.ReadAllBytes(@path + "\\archivo.pfx");
+
+            //Ejecutar comandos para generar archivo pfx
+            System.Diagnostics.Process proc = new System.Diagnostics.Process();
+
+            ProcessStartInfo info = new ProcessStartInfo("cmd.exe");
+            info.Arguments = "/C openssl pkcs12 -export -out CSD010_AAA010101AAA.pfx -in CSD010_AAA010101AAA.cer.pem -inkey CSD010_AAA010101AAA.key.pem -password pass:12345678a";
+            Process.Start(info);
+            proc.StartInfo = info;
+            proc.Start();
+            proc.WaitForExit();
+            proc.Close();
+
+            byte[] fileBytes = File.ReadAllBytes(@path + "\\CSD010_AAA010101AAA.pfx");
             string base642 = Convert.ToBase64String(fileBytes);
-            webservice.pfxbase64 = base642;
+            pfxBase64 = base642;
 
             var acceso_servicio = new Servicios();
-            if (acceso_servicio.cancelar_cfdi(webservice.username, webservice.password, webservice.rfc, webservice.uuid, webservice.pfxbase64, webservice.pfxpassword) == true)
-                MessageBox.Show("Termino el envío a cancelación del CFDI");
+
+            if (!string.IsNullOrEmpty(userName) && !string.IsNullOrEmpty(password) && !string.IsNullOrEmpty(rfcTextBox) && !string.IsNullOrEmpty(uuidTextBox) && !string.IsNullOrEmpty(pfxBase64) && !string.IsNullOrEmpty(pfxPassword))
+            {
+                respuesta_cancelar = acceso_servicio.cancelar_cfdi(userName, password, rfcTextBox, uuidTextBox, pfxBase64, pfxPassword);
+                MessageBox.Show(respuesta_cancelar, "Cancelar CFDI");
+            }
             else
-                MessageBox.Show("Hubo un error en la petición, revisar la consola para mas información");
+                MessageBox.Show("Por favor capture el RFC del emisor & UUID del CFDI", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
         }
     }
 }
--- ejemplo-.net/Servicios.cs
+++ ejemplo-.net/Servicios.cs
@@ -1,16 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
+using System.Xml.Xsl;
 
 namespace Main
 {
-    class Servicios
+    public class Servicios
     {
-
-        public bool timbrar_cfdi(string user_name, string password, string xml_base64)
+        public string timbrar_cfdi(string user_name, string password, string xml_base64)
         {
             try
             {
@@ -20,16 +22,17 @@ namespace Main
                 XmlDocument cfdi_timbrado = new XmlDocument();
                 cfdi_timbrado.LoadXml(response.xml);
                 Console.WriteLine(response.xml);
-                return true;
+
+                return response.xml;
             }
-            catch (Exception e)
+            catch (System.ServiceModel.FaultException e)
             {
-                Console.WriteLine(e);
-                return false;
+                Console.WriteLine("Código de error " + e.Code.Name + ": " + e.Message);
+                return "Código de error: " + e.Code.Name + "\n" + e.Message;
             }
         }
 
-        public bool cancelar_cfdi(string user_name, string password, string rfc, string uuid, string pfx_base64, string pfx_password)
+        public string cancelar_cfdi(string user_name, string password, string rfc, string uuid, string pfx_base64, string pfx_password)
         {
             try
             {
@@ -43,13 +46,76 @@ namespace Main
                 acuse_cancelacion.LoadXml(response.acuse_cancelacion);
                 XmlDocument comprobantes_cancelados = new XmlDocument();
                 comprobantes_cancelados.LoadXml(response.comprobantes_cancelados);
-                Console.WriteLine(response.ToString());
-                return true;
+                Console.WriteLine(response.comprobantes_cancelados.ToString());
+
+                return response.comprobantes_cancelados.ToString();
+            }
+            catch (System.ServiceModel.FaultException e)
+            {
+                Console.WriteLine("Código de error " + e.Code.Name + ": " + e.Message);
+                return "Código de error: " + e.Code.Name + "\n" + e.Message;
+            }
+        }
+
+        public string sellar_cfdi(string archivo, string path)
+        {
+            try
+            {
+                XmlDocument doc_xml = new XmlDocument();
+                //Se carga archivo
+                doc_xml.Load(archivo);
+
+                XmlNode root = doc_xml.DocumentElement;
+                XmlNamespaceManager manager = new XmlNamespaceManager(doc_xml.NameTable);
+                manager.AddNamespace("ns", "http://www.sat.gob.mx/cfd/3");
+                XmlNode node = doc_xml.DocumentElement.SelectSingleNode("//ns:Comprobante", manager);
+
+                //Se agrega la fecha actual al momento de generar cfdi
+                string fecha = node.Attributes.GetNamedItem("fecha").Value;
+                string fecha_hoy = DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ss");
+                node.Attributes.GetNamedItem("fecha").Value = fecha_hoy;
+                doc_xml.Save(archivo);
+
+                XslCompiledTransform xsl = new XslCompiledTransform();
+                xsl.Load(@path + "//cadenaoriginal_3_2.xslt");
+                XmlTextWriter xmlwritter = new XmlTextWriter(@path + "//cadena_original.txt", null);
+                xsl.Transform(archivo, null, xmlwritter);
+                string private_key = File.ReadAllText(@path + "//CSD010_AAA010101AAA.key.pem");
+
+                //Ejecutar comandos para obtener obtener el sello
+                System.Diagnostics.Process proc = new System.Diagnostics.Process();
+
+                ProcessStartInfo info = new ProcessStartInfo("cmd.exe");
+                info.Arguments = "/C openssl dgst -sha1 -sign CSD010_AAA010101AAA.key.pem -out digest.txt cadena_original.txt";
+                Process.Start(info);
+                proc.StartInfo = info;
+                proc.Start();
+                proc.WaitForExit();
+                proc.Close();
+
+                ProcessStartInfo info2 = new ProcessStartInfo("cmd.exe");
+                info2.Arguments = "/C openssl enc -in digest.txt -out sello.txt -base64 -A -K CSD010_AAA010101AAA.key.pem";
+                Process.Start(info2);
+                proc.StartInfo = info2;
+                proc.Start();
+                proc.WaitForExit();
+                proc.Close();
+
+                //Una vez obtenido el sello lo incrustamos
+                string sello = File.ReadAllText(@path + "//sello.txt");
+                node.Attributes.GetNamedItem("sello").Value = sello;
+                doc_xml.Save(archivo);
+
+                //conversion de xml a base64
+                byte[] base64 = Encoding.UTF8.GetBytes(doc_xml.InnerXml);
+                string convertXml = Convert.ToBase64String(base64);
+
+                return convertXml;
             }
             catch (Exception e)
             {
                 Console.WriteLine(e);
-                return false;
+                return string.Empty;
             }
         }
     }
